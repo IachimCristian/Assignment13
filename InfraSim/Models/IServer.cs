e2430feb12554d2dd0598ba90f472ce82769bc14
Iachim Cristian <2238224@student.uma.pt>
2025-03-16 19:09:49 +0000

    Created the Core Interfaces and Classes

---
+++ InfraSim/Models/IServer.cs
@@ -0,0 +1,14 @@
+namespace InfraSim.Models
+{
+    /// <summary>
+    /// Interface for server components that can handle requests
+    /// </summary>
+    public interface IServer
+    {
+        /// <summary>
+        /// Handles a specified number of requests
+        /// </summary>
+        /// <param name="requestsCount">The number of requests to handle</param>
+        void HandleRequests(int requestsCount);
+    }
+} 
