3210a53ad853dd9b9516c785280a78add4100a33
Iachim Cristian <2238224@student.uma.pt>
2025-03-17 20:23:04 +0000

    Validated My Implementation with Unit Tests

--- InfraSim.Tests/CapabilityFactoryTests.cs
+++ InfraSim.Tests/CapabilityFactoryTests.cs
@@ -13,69 +13,20 @@ namespace InfraSim.Tests
             _factory = new CapabilityFactory();
         }
 
-        [Fact]
-        public void Create_RegularServer_ReturnsBaseCapability()
+        [Theory]
+        [InlineData(ServerType.WebServer, typeof(ServerCapability), 1000, 2500)]
+        [InlineData(ServerType.CacheServer, typeof(TemporaryStorageCapability), 100, 3500)]
+        [InlineData(ServerType.LoadBalancer, typeof(TrafficDistributionCapability), 10000, 4000)]
+        [InlineData(ServerType.CDN, typeof(EdgeServerCapability), 1000, 55000)]
+        [InlineData(ServerType.DatabaseServer, typeof(ServerCapability), 1000, 2500)]
+        public void Create_ServerType_ReturnsExpectedCapability(ServerType serverType, Type expectedType, long expectedRequests, int expectedCost)
         {
-            var serverType = ServerType.WebServer;
-            
             var capability = _factory.Create(serverType);
             
             Assert.NotNull(capability);
-            Assert.IsType<ServerCapability>(capability);
-            Assert.Equal(1000, capability.MaximumRequests);
-            Assert.Equal(2500, capability.Cost);
-        }
-
-        [Fact]
-        public void Create_CacheServer_ReturnsTemporaryStorageCapability()
-        {
-            var serverType = ServerType.CacheServer;
-            
-            var capability = _factory.Create(serverType);
-            
-            Assert.NotNull(capability);
-            Assert.IsType<TemporaryStorageCapability>(capability);
-            Assert.Equal(100, capability.MaximumRequests);
-            Assert.Equal(3500, capability.Cost);
-        }
-
-        [Fact]
-        public void Create_LoadBalancer_ReturnsTrafficDistributionCapability()
-        {
-            var serverType = ServerType.LoadBalancer;
-            
-            var capability = _factory.Create(serverType);
-            
-            Assert.NotNull(capability);
-            Assert.IsType<TrafficDistributionCapability>(capability);
-            Assert.Equal(10000, capability.MaximumRequests);
-            Assert.Equal(4000, capability.Cost);
-        }
-
-        [Fact]
-        public void Create_CDN_ReturnsFullyDecoratedCapability()
-        {
-            var serverType = ServerType.CDN;
-            
-            var capability = _factory.Create(serverType);
-            
-            Assert.NotNull(capability);
-            Assert.IsType<EdgeServerCapability>(capability);
-            Assert.Equal(1000, capability.MaximumRequests);
-            Assert.Equal(55000, capability.Cost);
-        }
-
-        [Fact]
-        public void Create_UnknownServerType_ReturnsBaseCapability()
-        {
-            var serverType = ServerType.DatabaseServer;
-            
-            var capability = _factory.Create(serverType);
-            
-            Assert.NotNull(capability);
-            Assert.IsType<ServerCapability>(capability);
-            Assert.Equal(1000, capability.MaximumRequests);
-            Assert.Equal(2500, capability.Cost);
+            Assert.IsType(expectedType, capability);
+            Assert.Equal(expectedRequests, capability.MaximumRequests);
+            Assert.Equal(expectedCost, capability.Cost);
         }
     }
 } 
