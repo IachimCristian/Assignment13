3148082b730b11e4b33fff5a71cc9a83e92c426b
Iachim Cristian <2238224@student.uma.pt>
2025-03-17 20:28:19 +0000

    Final touches

--- InfraSim.Tests/CapabilityFactoryTests.cs
+++ InfraSim.Tests/CapabilityFactoryTests.cs
@@ -4,19 +4,19 @@ using InfraSim.Models;
 
 namespace InfraSim.Tests
 {
-    public class CapabilityFactoryTests
+    public class CapabilityFactoryTests // TODO: Add tests for the CapabilityFactory class
     {
-        private readonly ICapabilityFactory _factory;
+        private readonly ICapabilityFactory _factory; 
 
-        public CapabilityFactoryTests()
+        public CapabilityFactoryTests() 
         {
-            _factory = new CapabilityFactory();
+            _factory = new CapabilityFactory(); 
         }
 
-        [Theory]
-        [InlineData(ServerType.WebServer, typeof(ServerCapability), 1000, 2500)]
-        [InlineData(ServerType.CacheServer, typeof(TemporaryStorageCapability), 100, 3500)]
-        [InlineData(ServerType.LoadBalancer, typeof(TrafficDistributionCapability), 10000, 4000)]
+        [Theory] 
+        [InlineData(ServerType.WebServer, typeof(ServerCapability), 1000, 2500)] // TODO: Add tests for the WebServer type
+        [InlineData(ServerType.CacheServer, typeof(TemporaryStorageCapability), 100, 3500)] // TODO: Add tests for the CacheServer type
+        [InlineData(ServerType.LoadBalancer, typeof(TrafficDistributionCapability), 10000, 4000)] // TODO: Add tests for the LoadBalancer type
         [InlineData(ServerType.CDN, typeof(EdgeServerCapability), 1000, 55000)]
         [InlineData(ServerType.DatabaseServer, typeof(ServerCapability), 1000, 2500)]
         public void Create_ServerType_ReturnsExpectedCapability(ServerType serverType, Type expectedType, long expectedRequests, int expectedCost)
