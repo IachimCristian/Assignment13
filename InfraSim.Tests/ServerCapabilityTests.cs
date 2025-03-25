56066b8d8958a1798c5fbce84cd72cbf1b3983a1
Iachim Cristian <2238224@student.uma.pt>
2025-03-25 01:40:20 +0000

    Final touches

--- InfraSim.Tests/ServerCapabilityTests.cs
+++ InfraSim.Tests/ServerCapabilityTests.cs
@@ -6,10 +6,10 @@ namespace InfraSim.Tests
     public class ServerCapabilityTests
     {
         [Theory]
-        [InlineData(ServerType.Server, 1000, 2500)]
-        [InlineData(ServerType.Cache, 100000, 3500)]
-        [InlineData(ServerType.LoadBalancer, 10000000, 4000)]
-        [InlineData(ServerType.CDN, 1000000000000, 55000)]
+        [InlineData(ServerType.Server, 1000, 2500)] // Base case 
+        [InlineData(ServerType.Cache, 100000, 3500)] // Cache has a temporary storage decorator
+        [InlineData(ServerType.LoadBalancer, 10000000, 4000)] // Load balancer has a traffic distribution decorator 
+        [InlineData(ServerType.CDN, 1000000000000, 55000)] // CDN has a temporary storage decorator  
         public void Create_ReturnsCorrectCapability(ServerType type, long expectedRequests, int expectedCost)
         {
             var factory = new ServerCapability();
