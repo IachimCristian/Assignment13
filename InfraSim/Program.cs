2605f5c8be0019ced032e067d6a09886d834f97f
Iachim Cristian <2238224@student.uma.pt>
2025-03-30 13:56:04 +0100

    Used Singleton and Dependency Injection

--- InfraSim/Program.cs
+++ InfraSim/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using InfraSim.Models.Capability;
+using InfraSim.Models.Server;
 
 namespace InfraSim
 {
@@ -21,6 +24,10 @@ namespace InfraSim
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IServerCapability, ServerCapability>();
                 });
     }
 }
