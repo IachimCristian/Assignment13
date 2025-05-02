cf17afa3eef73c3ec0dcc7296e75ff930be45fa9
Iachim Cristian <2238224@student.uma.pt>
2025-05-02 14:39:51 +0100

    Final touches

--- InfraSim.Tests/CommandManagerTests.cs
+++ InfraSim.Tests/CommandManagerTests.cs
@@ -5,10 +5,10 @@ using Xunit;
 
 namespace InfraSim.Tests
 {
-    public class CommandManagerTests
+    public class CommandManagerTests // Tests for Command Manager 
     {
         [Fact]
-        public void Execute_CallsDo()
+        public void Execute_CallsDo() // Tests for Do
         {
             var mockCommand = new Mock<ICommand>();
             var manager = new CommandManager();
@@ -17,7 +17,7 @@ namespace InfraSim.Tests
         }
 
         [Fact]
-        public void Undo_CallsUndoOnLastCommand()
+        public void Undo_CallsUndoOnLastCommand() // Tests for Undo
         {
             var mockCommand = new Mock<ICommand>();
             var manager = new CommandManager();
@@ -27,7 +27,7 @@ namespace InfraSim.Tests
         }
 
         [Fact]
-        public void Redo_CallsRedoOnNextCommand()
+        public void Redo_CallsRedoOnNextCommand() // Tests for Redo
         {
             var mockCommand = new Mock<ICommand>();
             var manager = new CommandManager();
